e7e0cf57e368bb50c9309498686bb4d8ce0466d6
Motolani <alebiosumotolani@gmail.com>
2025-11-09 23:10:41 +0100

    update to contain RowElement and RowId

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,6 +1,5 @@
-/// <reference types="node" />
 import { RowID, RowElement } from './interface';
 
-export declare function insertRow(row: RowElement): RowID;
-export declare function deleteRow(rowId: RowID): void;
-export declare function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
