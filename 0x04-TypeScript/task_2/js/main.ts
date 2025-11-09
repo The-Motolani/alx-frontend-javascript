9ba8bdba0a5360f8669cf55d46ee179417e2659f
Motolani <alebiosumotolani@gmail.com>
2025-11-09 22:55:41 +0100

    Advanced types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,56 +1,75 @@
-// DirectorInterface
+//  Interfaces
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
-// TeacherInterface
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
 
-//  Director class implementing DirectorInterface
+//  Classes
 class Director implements DirectorInterface {
   workFromHome(): string {
-    return "Working from home";
+    return 'Working from home';
   }
 
   getCoffeeBreak(): string {
-    return "Getting a coffee break";
+    return 'Getting a coffee break';
   }
 
   workDirectorTasks(): string {
-    return "Getting to director tasks";
+    return 'Getting to director tasks';
   }
 }
 
-// Teacher class implementing TeacherInterface
 class Teacher implements TeacherInterface {
   workFromHome(): string {
-    return "Cannot work from home";
+    return 'Cannot work from home';
   }
 
   getCoffeeBreak(): string {
-    return "Cannot have a break";
+    return 'Cannot have a break';
   }
 
   workTeacherTasks(): string {
-    return "Getting to work";
+    return 'Getting to work';
   }
 }
 
-// createEmployee function
+//  createEmployee Function
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+  if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   } else {
     return new Director();
   }
 }
 
-// Test output
-console.log(createEmployee(200));   // Teacher
-console.log(createEmployee(1000)); // Director
+// isDirector function
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+//  executeWork function
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// String literal type and teachClass
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
